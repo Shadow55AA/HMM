70065892b15e3638f241436b5bd66a753574eeab
ShadowAA55 <alozensky@yahoo.com>
2021-12-23 18:17:36 -0600

    PixelDisplay
    Added PixelDisplay and language files.

---
+++ HMM/shared/IPixelDisplayData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMM.Shared
+{
+    public interface IPixelDisplayData
+    {
+        int SizeX { get; set; }
+        int SizeZ { get; set; }
+        byte[] memdata { get; set; }
+    }
+}
